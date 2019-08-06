fa132ab348ee74e2c20809f75f759c875e07afdc
mmwanje <mwanjemusa@gmail.com>
2019-08-06 12:50:50 +0300

    first commit challenge 2 alc

---
+++ app/src/main/java/com/example/travelmantics/MainActivity.java
@@ -0,0 +1,195 @@
+package com.example.travelmantics;
+
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.data.model.Resource;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+
+    EditText txtTile, txtDescription, txtPrice;
+    String title, price, description;
+    TravelDeal deal;
+    Button btnImage;
+    ImageView imageView;
+    private static final int PICTURE_RESULT = 42;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // FirebaseUtil.openFbReference("traveldeals", );
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatebase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        txtTile = (EditText) findViewById(R.id.txtName);
+        txtPrice = (EditText) findViewById(R.id.txtPrice);
+        txtDescription = (EditText) findViewById(R.id.txtDesc);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        btnImage = (Button) findViewById(R.id.btnImage);
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(intent.createChooser(intent, "Insert Picture"),PICTURE_RESULT);
+            }
+        });
+
+        Intent intent = getIntent();
+        TravelDeal deal = (TravelDeal) intent.getSerializableExtra("Deal");
+        if (deal==null){
+            deal = new TravelDeal();
+        }
+        this.deal = deal;
+        txtTile.setText(deal.getTitle());
+        txtDescription.setText(deal.getDescription());
+        txtPrice.setText(deal.getPrice());
+        showImage(deal.getImageUrl());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this,"Deal Saved", Toast.LENGTH_LONG).show();
+                clean();
+                backToList();
+                return  true;
+            case R.id.delete_menu:
+                deleteDeal();
+                Toast.makeText(this,"Deal Deleted", Toast.LENGTH_LONG).show();
+                backToList();
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        if (FirebaseUtil.isAdmin){
+            menu.findItem(R.id.delete_menu).setVisible(true);
+            menu.findItem(R.id.save_menu).setVisible(true);
+            enableEditTexts(true);
+        }else {
+            menu.findItem(R.id.delete_menu).setVisible(false);
+            menu.findItem(R.id.save_menu).setVisible(false);
+            enableEditTexts(false);
+        }
+        return  true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode,data);
+        if (requestCode == PICTURE_RESULT && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            StorageReference ref = FirebaseUtil.mStorageRef.child(imageUri.getLastPathSegment());
+            /*ref.putFile(imageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //String url = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                    String url = taskSnapshot.getStorage().getDownloadUrl().toString();
+                    Log.e("url", "" + url);
+                    deal.setImageUrl(url);
+                    showImage(url);
+
+                }
+            });*/
+
+            ref.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> task = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                    task.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = uri.toString();
+                            deal.setImageUrl(url);
+                            Toast.makeText(getApplicationContext(),url,Toast.LENGTH_LONG).show();
+                            showImage(url);
+                        }
+                    });
+                }
+            });
+
+
+
+        }
+    }
+
+    private void saveDeal(){
+
+        deal.setTitle(txtTile.getText().toString());
+        deal.setPrice(txtPrice.getText().toString());
+        deal.setDescription(txtDescription.getText().toString());
+        if (deal.getId() == null){
+            mDatabaseReference.push().setValue(deal);
+        }else {
+            mDatabaseReference.child(deal.getId()).setValue(deal);
+        }
+    }
+    private void deleteDeal(){
+        if (deal == null){
+            Toast.makeText(this,"Please Save the deal before delecting",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mDatabaseReference.child(deal.getId()).removeValue();
+    }
+    public void backToList(){
+        Intent intent = new Intent(this,ListActivity.class);
+        startActivity(intent);
+    }
+    private void clean(){
+        txtTile.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTile.requestFocus();
+
+    }
+    private void enableEditTexts(boolean isEnabled){
+        txtTile.setEnabled(isEnabled);
+        txtDescription.setEnabled(isEnabled);
+        txtPrice.setEnabled(isEnabled);
+    }
+    private void showImage(String url){
+        if (url != null && url.isEmpty() == false){
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            Picasso.get()
+                    .load(url)
+                    .resize(width,width* 2/3)
+                    .centerCrop()
+                    .into(imageView);
+        }
+    }
+}
